384a5d614983b4ab0cbd12d0bb7425b328410921
kanno03 <jkkikani2003@gmail.com>
2023-08-12 11:27:33 +0530

    first commit

---
+++ hist.js
@@ -0,0 +1,62 @@
+let historybutton = document.getElementById('historybutton');
+let history = document.getElementById('history');
+let bar1 = document.getElementById('bar1');
+let bar2 = document.getElementById('bar2');
+let dis=document.getElementById('answer');
+
+function showHistory() {
+    let calcHistory = JSON.parse(localStorage.getItem("calcHistory")) || [];
+    let len = calcHistory.length;
+
+    history.innerHTML = '';
+
+    bar1.style.display = 'block';
+    bar2.style.display = 'block';
+    if (len === 0) {
+        let historyItem = document.createElement('div');
+        historyItem.innerHTML = "There's no history yet.";
+        historyItem.className = 'historyelement his';
+        historyItem.style.fontSize = '25px';
+        history.appendChild(historyItem);
+    } else {
+        for (let index = len-1; index >=0; index--) {
+            const element = calcHistory[index];
+            let historyItem = document.createElement('div');
+            historyItem.className = 'historyelement';
+            historyItem.innerHTML = `${element.lastScreenValue} = <span style="color: ${element.result < 0 ? 'red' : 'green'}">${element.result}</span>`;//Actually I have added this that makes red color in the history section .............
+            history.appendChild(historyItem);
+            if (index > 0) history.appendChild(document.createElement('hr'));
+        }
+    }
+
+    history.style.display = 'block';
+}
+
+historybutton.addEventListener('click', showHistory);
+
+function clearAll(){
+  if(dis.value=0){
+  screenValue = screenValue.slice(0, -1);
+  screen.value = screenValue;
+}  else(dis.value='');     
+}
+
+function hide(){
+    history.style.display = 'none';
+    bar1.style.display = 'none';
+    bar2.style.display = 'none';
+}
+function deleteLastEntry() {
+    let calcHistory = JSON.parse(localStorage.getItem("calcHistory")) || [];
+    if (calcHistory.length > 0) {
+      calcHistory.pop(); 
+      localStorage.setItem("calcHistory", JSON.stringify(calcHistory));
+      showHistory();
+    }
+    else(dis.value='');
+  }
+
+
+
+bar1.addEventListener('click', hide);
+bar2.addEventListener('click', hide);
